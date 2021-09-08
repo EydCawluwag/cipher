412ec62b86f86996edfcdb0ad67d28b1130267d6
shesh <79000021+carlo-vbs@users.noreply.github.com>
2021-09-08 14:43:58 +0800

    updated ceasar.py file
    change function names, modified encrypt function, and fix the first bug it have which is whenever the shift goes above a certain value the program can't solve it cause the program waaaaaahhh lisod explaain basta bug siya!

--- ceasar-cipher/ceasar.py
+++ ceasar-cipher/ceasar.py
@@ -1,29 +1,20 @@
 
-def encryptMod(stringToEnc,shift):
+def enc(stringToEnc,shift):
 	alphabet,brokenString,out=list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.lower()),stringToEnc.split(' '),''
 	for i in brokenString:
 		a=list(i.lower())
 		for j in range(len(a)):
 			if a[j] in alphabet:
 				b=alphabet.index(a[j].lower())
-				if (b+shift)>len(alphabet)-1: b=(b+shift)-len(alphabet)
+				if (b+shift)>len(alphabet)-1: b=(b+shift)%len(alphabet)
 				else:b=b+shift
 				out+=alphabet[b]
 			else:
 				out+=a[j]
 		out+=' '
 	return out
-def encrypt(stringToEnc):
-	alphabet,brokenString,out=list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.lower()),stringToEnc.split(' '),''
-	for i in brokenString:
-		a=list(i)
-		for j in range(len(a)):
-			b=alphabet.index(a[j].lower())
-			if (b+3)>len(alphabet)-1: b=(b+3)-len(alphabet)
-			else:b=b+3
-			out+=alphabet[b]
-		out+=' '
-	return out
+def encDef(stringToEnc):
+	return encryptMod(stringToEnc,3)
 
 
 
