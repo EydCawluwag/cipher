6237fd09c899e1aed25e16a321110531310d1cde
shesh <79000021+carlo-vbs@users.noreply.github.com>
2021-09-13 09:02:53 +0800

    updated alberti.py

--- alberti-cipher/alberti.py
+++ alberti-cipher/alberti.py
@@ -1,10 +1,15 @@
 stabilis=list('ABCDEFGILMNOPQRSTVXZ1234')
 mobilis=list('gklnprtuz&xysomqihfdbace')
+initialShift=0
+periodicIncrement=1
+periodLength=4
+
 
 def shift(arrToShift,shift):
 	if shift>len(arrToShift):return algShift(arrToShift,shift%len(arrToShift))
 	return algShift(arrToShift,shift)
 
+# clockwise shift!!!
 def algShift(arrToShift,shift):
 	a,b,c,d=[],len(arrToShift),arrToShift,shift
 	[a.append(arrToShift[i]) for i in range(b-d,b)]
@@ -12,12 +17,14 @@ def algShift(arrToShift,shift):
 	return a
 
 def enc(stringToEnc):
-	print(stabilis)
-	print(shift(mobilis,1))
+	a,b,c,d,e,f=initialShift,periodicIncrement,periodLength,len(stringToEnc),mobilis,stabilis
+	for i in range(int(d/c)):
+		e=shift(e,b)
+		print(e)
 
 def dec(stringToDec):
 	pass
 
 
 
-enc('LouiseCarlo')
+enc('LouiseCarloSalomonGwapoKaayu')
